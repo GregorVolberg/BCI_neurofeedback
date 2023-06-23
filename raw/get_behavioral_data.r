1f3e85ab240e79130ccb7ce78dc424150a0f9e9a
Gregor Volberg <66525570+GregorVolberg@users.noreply.github.com>
2023-06-23 21:02:23 +0200

    descriptives and stats

--- raw/get_behavioral_data.r
+++ raw/get_behavioral_data.r
@@ -1,6 +1,6 @@
 library(tidyverse)
-library(lubridate) # for time / date data type; you might need to install that package
-library(R.matlab)  # for reading matlab *.mat-files into R; you might need to install that package
+library(lubridate) # for time / date data type; package is part of tidyverse
+library(R.matlab)  # for reading Matlab's *.mat-files into R; you might need to install that package
 
 fileListBehavioral <- c("AL1_BCIcar.mat", "AW7_BCIcar.mat", "DS3_BCIcar.mat", "JG1_BCIcar.mat")
 
@@ -21,7 +21,6 @@ get_rawDataBehavioral <- function(fileName){
              obstacle = fct_recode(obstacle,
                                    left  = "-1",
                                    right = "1"),
-             #outcome = V4,
              outcome = as_factor(V4),
              outcome = fct_recode(outcome,
                                   pass  = "1",
@@ -31,14 +30,45 @@ get_rawDataBehavioral <- function(fileName){
       select(vp, trial, speed, ttc, obstacle, outcome, experiment, date)    
 }
 
-tst <- get_rawDataBehavioral(fileList[1])
-tib <- map_df(fileList, get_rawDataBehavioral) # tibble with all participants
+# apply function to all raw data files and concatenate to tibble
+allVp <- map_df(fileListBehavioral, get_rawDataBehavioral)
 
-# max und min ttc rausschreiben, damit datenvektor für alle gleich ist
-# Achtung, V2 korrigieren, schreibt 1 zu wenig raus bei fail (V2 ist bei fail V2+1)
-behavRes <-  tib %>%
+# compute cumulative probability for success at increasing levels of ttc 
+behavResults <-  allVp %>%
              group_by(vp, ttc) %>%
-             summ  
-  
-plot(cumsum(table(tst$ttc, tst$outcome)[,2]) / sum(table(tst$ttc, tst$outcome)))
+             summarize(n       = n(),
+                       n_pass  = sum(outcome == 'pass')) %>%
+              group_by(vp)  %>%
+              mutate(cumsum_pass  = cumsum(n_pass),
+                     cumprop_pass = cumsum_pass / sum(n)) %>%
+              select(vp, ttc, n, n_pass, cumprop_pass)
 
+# aggregate across participants
+grandMean = behavResults %>%
+            group_by(ttc) %>%
+            summarize(m  = mean(cumprop_pass),
+                      se = sd(cumprop_pass)/sqrt(n())) %>%
+            ungroup()
+
+# plot descriptives
+plot(NULL,
+     ylim = c(0,1), xlim = c(300, 1600), xaxt = 'n',
+     ylab = 'Cumulative p passed', xlab = 'Time to collision (s)')
+axis(1, at = seq(400, 1600, 200), labels = seq(0.4, 1.6, 0.2))
+xpoly <- c(grandMean$ttc, rev(grandMean$ttc))
+se <- grandMean$se
+se[is.na(se)] = 0
+ypoly <- c(grandMean$m + se, rev(grandMean$m) - rev(se))
+polygon(xpoly, ypoly, col = 'lightblue', border = 'lightblue')
+lines(grandMean$m ~ grandMean$ttc, lwd=2, type = 'line', col= 'blue')
+legend(x=1000, y=0.4, legend = c('behavioral', 'BCI-EEG'), lty = 'solid', lwd = 2, col = c('blue', 'orange'), bty='n')
+
+# statistics; e.g., dependent t-tests on pre-defined ttc quantiles between behavioral and BCI data
+
+allVp %>% 
+  filter(outcome == 'pass') %>%
+  group_by(vp) %>%
+  summarize(q1  = quantile(ttc, 0.25),
+            q2  = quantile(ttc, 0.50),
+            q3  = quantile(ttc, 0.75))
+ # to be continued
