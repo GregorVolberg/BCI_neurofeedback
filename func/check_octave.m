1f3b840065a3276075398bbb7e69b5759439eceb
Gregor Volberg <gregor.volberg@ur.de>
2024-04-23 10:01:47 +0200

    SoSe2024

--- func/check_octave.m
+++
@@ -1,7 +0,0 @@
-function [isOctave] = check_octave()
-isOctave = exist('OCTAVE_VERSION', 'builtin') ~= 0;
-if isOctave
-pkg load statistics
-pkg load signal
-end
-end
--- func/get_luminance_from_sine.m
+++
@@ -1,3 +0,0 @@
-function [lum] = get_luminance_from_sine(f, t, phi0)
-   lum = (sin(2*pi*f*t+phi0) +1)/2;
-  end
