7357ec6b8dda41f60cda9aa0f3638b4375800134
Gregor Volberg <gregor.volberg@ur.de>
2024-05-16 15:56:42 +0200

    lesson1

--- overleafBrainflow.r
+++ overleafBrainflow.r
@@ -13,12 +13,41 @@ myboard$start_stream()    # start stream
 myboard$stop_stream()     # stop stream
 myboard$release_session() # end session
   
+# board info, query before starting the session
+info <- myboard$get_board_descr(Id)
+info$sampling_rate 
+info$eeg_channels   # add 1
+info$marker_channel # add 1
+
+# polling (between start and stop stream)
+nSamp = as.integer(250)
+myboard$get_current_board_data(nSamp)
+m=myboard$get_board_data(nSamp)
+myboard$get_board_data_count()
+
+## save file to disk
+myboard$add_streamer('file://myfile.csv:w')
+eeg <- read_csv('myfile.csv') # readr::read_csv
 
 ## to connect with Cyton board
 Id      <- brainflow_python$BoardIds$CYTON_BOARD 
 params  <- brainflow_python$BrainFlowInputParams()   
 params.serial_port <- "COM3" # subject to change
 myboard <- brainflow_python$BoardShim(Id, params) 
+
+# dummy playback board
+Id      <- brainflow_python$BoardIds$PLAYBACK_FILE_BOARD 
+params  <- brainflow_python$BrainFlowInputParams()   
+params.file <- "myfile.csv"
+params.master_board <- brainflow_python$BoardIds$SYNTHETIC_BOARD
+
+myboard <- brainflow_python$BoardShim(Id, params) 
+
+
+brainflow_python$BoardShim$release_all_sessions();
+
+
+myboard$get_current_board_data(as.integer(10), preset)
 
 ## dummy boards
 Id      <- brainflow_python$BoardIds$SYNTHETIC_BOARD 
