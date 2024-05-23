784b63c946279223057e728bc022691b00b844c3
Gregor Volberg <gregor.volberg@ur.de>
2024-05-23 16:27:51 +0200

    artifact reject methods

---
+++ artifact_detection.R
@@ -0,0 +1,13 @@
+# how to identify artifacts during polling
+# https://www.ncbi.nlm.nih.gov/pmc/articles/PMC2234090/
+# birbaumer
+https://docs.openbci.com/Examples/EEGProjects/MotorImagery/
+  
+https://www.sciencedirect.com/science/article/pii/S0165027021001849
+probability mapping from single channel eeg, J neurosci methods
+
+https://www.frontiersin.org/research-topics/28099/recent-advances-in-eeg-non-invasive-based-bci-applications/magazine
+
+https://www.sciencedirect.com/science/article/pii/S1053811917305013
+Gramfort 2017
+mit python bib, https://autoreject.github.io/stable/index.html
